a225d760333472c0a30948cb5044479039bcf1c3
Cam <cawoolf89@gmail.com>
2020-06-07 15:27:31 +0000

    Main activity and vcs changed.

--- app/src/main/java/com/example/androidgitintegration/MainActivity.java
+++ app/src/main/java/com/example/androidgitintegration/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("Commit","Committting");
         Log.i("Commit","Committting");
+        Log.i("Commit","Reverttt");
     }
 
     public static void gitStuff() {
